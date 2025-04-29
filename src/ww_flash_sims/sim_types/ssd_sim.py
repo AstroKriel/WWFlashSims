60f2b9ca59e3f6036539e2eec43e8002653ee57b
Neco Kriel <necokriel@gmail.com>
2025-04-29 11:57:34 +1000

    ignore extraneous files

--- src/ww_flash_sims/sim_types/ssd_sim.py
+++ src/ww_flash_sims/sim_types/ssd_sim.py
@@ -3,9 +3,14 @@ from pathlib import Path
 class SSDSimulation():
   ALIASES = {
     "N_res"         : "num_cells_per_box_length",
-    "scaled_k_turb" : "box_normalised_forcing_wave_number",
     "mach_0"        : "init_mach_number",
+    "E_ratio_0"     : "init_energy_ratio",
+    "E_kin_0"       : "init_kinetic_energy",
+    "E_mag_0"       : "init_magnetic_energy",
+    "scaled_k_turb" : "box_normalised_forcing_wave_number",
+    "k_turb_width"  : "forcing_wavenumber_width",
     "t_turb_0"      : "init_turnover_time",
+    "t_turb_max"    : "num_turnover_times",
     "ell_box"       : "box_length",
     "c_s"           : "sound_speed",
     "rho"           : "total_density",
@@ -21,9 +26,11 @@ class SSDSimulation():
       self,
       directory                          : str | Path,
       num_cells_per_box_length           : float,
-      box_normalised_forcing_wave_number : float,
-      init_energy_ratio                  : float,
       init_mach_number                   : float,
+      init_energy_ratio                  : float = 1e-10,
+      box_normalised_forcing_wave_number : float = 2.0,
+      forcing_fourier_profile            : str = "parabolic",
+      forcing_wavenumber_width           : float = 1.0,
       dumps_per_turnover_time            : int = 2,
       num_turnover_times                 : float = 100.0,
       box_length                         : float = 1.0,
@@ -38,13 +45,15 @@ class SSDSimulation():
       num_blocks                         : tuple[int, int, int] | None = None,
       num_cells_per_block                : tuple[int, int, int] | None = None,
     ):
-    self.directory                          = directory
+    self.directory                          = Path(directory).absolute()
     self.num_cells_per_box_length           = num_cells_per_box_length
-    self.box_normalised_forcing_wave_number = box_normalised_forcing_wave_number
+    self.init_mach_number                   = init_mach_number
     self.init_energy_ratio                  = init_energy_ratio
     self.init_kinetic_energy                = None
     self.init_magnetic_energy               = None
-    self.init_mach_number                   = init_mach_number
+    self.box_normalised_forcing_wave_number = box_normalised_forcing_wave_number
+    self.forcing_fourier_profile            = forcing_fourier_profile
+    self.forcing_wavenumber_width           = forcing_wavenumber_width
     self.init_turnover_time                 = None
     self.dumps_per_turnover_time            = dumps_per_turnover_time
     self.num_turnover_times                 = num_turnover_times
@@ -62,12 +71,35 @@ class SSDSimulation():
     self.num_blocks                         = num_blocks
     self.num_cells_per_block                = num_cells_per_block
 
-  def check_all_params_are_defined(self):
-    undefined_vars = []
+  def compute_missing_params(self):
+    self._compute_missing_init_conditions()
+    self._compute_missing_plasma_numbers()
+    self._compute_grid_properties()
+    self._check_all_params_are_defined()
+    self._check_all_params_are_valid()
+
+  def print_sim_params(self):
+    reverse_aliases = {value: key for key, value in self.ALIASES.items()}
+    entries = []
     for var_name, var_value in self.__dict__.items():
-      if var_value is None: undefined_vars.append(var_name)
-    if len(undefined_vars) > 0:
-      raise ValueError(f"The following parameters have not been defined: {undefined_vars}")
+        alias_name = reverse_aliases.get(var_name, "")
+        entries.append((
+          f"`{var_name}`",
+          f"(alias: `{alias_name}`)" if alias_name else "",
+          var_value,
+          type(var_value).__name__
+        ))
+    entries.sort(key=lambda entry: entry[0])
+    max_var_name_len   = max(len(var_name) for var_name, _, _, _ in entries)
+    max_alias_name_len = max(len(alias_name) for _, alias_name, _, _ in entries)
+    max_value_len      = max(len(str(value)) for _, _, value, _ in entries)
+    print("SSD Simulation Properties:\n")
+    for var_name, alias_name, var_value, var_type in entries:
+      var_str   = f"{var_name:<{max_var_name_len}}"
+      alias_str = f"{alias_name:<{max_alias_name_len}}" if alias_name else " " * max_alias_name_len
+      value_str = f"{str(var_value):>{max_value_len}}"
+      print(f"\t- {var_str} {alias_str} = {value_str} [{var_type}]")
+
 
   @classmethod
   def _alias(cls, alias_name, var_name):
@@ -111,10 +143,20 @@ class SSDSimulation():
     elif self.N_res in [ 144, 288 ]:  self.num_blocks = (36, 36, 48)
     elif self.N_res in [ 36, 72 ]:    self.num_blocks = (12, 12, 18)
     elif self.N_res in [ 18 ]:        self.num_blocks = (6, 6, 6)
-    else: raise ValueError
+    else: raise ValueError(f"Simulation resolution = `{self.Nres:.d}` is not supported.")
     self.num_cells_per_block = tuple(
       int(self.N_res / num_blocks_in_dir)
       for num_blocks_in_dir in self.num_blocks
     )
+
+  def _check_all_params_are_valid(self):
+    if self.scaled_k_turb < 1.0: raise ValueError("Forcing mode should be smaller than the simulation box.")
+
+  def _check_all_params_are_defined(self):
+    undefined_vars = []
+    for var_name, var_value in self.__dict__.items():
+      if var_value is None: undefined_vars.append(var_name)
+    if len(undefined_vars) > 0:
+      raise ValueError(f"The following parameters have not been defined: {undefined_vars}")
 
 SSDSimulation._create_aliases()
